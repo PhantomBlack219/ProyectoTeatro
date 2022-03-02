dfd49c5ae2708f1dad666cdb3551a0d1b6426d0b
DMCronos <94533347+DMCronos@users.noreply.github.com>
2022-03-02 00:56:39 -0500

    Ingreso de información, arreglo visual del login
    Se metió un poco de información para la clase y se mejoró el login

--- Teatro/teatro/obras/views.py
+++ Teatro/teatro/obras/views.py
@@ -16,3 +16,23 @@ def obras(request):
     template = loader.get_template('oteatro/obras.html')
     context = {}
     return HttpResponse(template.render(context,request))
+
+def quienessomos(request):
+    template = loader.get_template('Quienes_somos/QuienesSomos.html')
+    context = {}
+    return HttpResponse(template.render(context,request))
+
+def blog(request):
+    template = loader.get_template('blog/blogpost')
+    context = {}
+    return HttpResponse(template.render(context,request))
+
+def bloghome(request):
+    template = loader.get_template('blog/bloghome')
+    context = {}
+    return HttpResponse(template.render(context,request))
+
+def blog1(request):
+    template = loader.get_template('blog/blog1')
+    context = {}
+    return HttpResponse(template.render(context,request))
